c05fbb4d9a9372e774397845d898a089dbb84c87
Johan Burger <jdpburger@gmail.com>
2024-03-24 13:59:58 +0100

    Show holidays for hard coded parameters

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+import holidays
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html', message='Hello World!')
+def holiday_list():
+    list = sorted(holidays.country_holidays("DE", "HE", years=2024).items())
+    return render_template('index.html', list=list)
